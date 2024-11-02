7fc3248266a4f33e6f48ff4b1292cce73e7ac05f
Nathanont <118962651+EclipseNonZ@users.noreply.github.com>
2024-11-02 12:10:47 +0700

    Add files via upload

---
+++ can.py
@@ -0,0 +1,40 @@
+import math
+
+
+def Area(r,v):
+    return 2*math.pi*pow(r,2)+ 2*v/r
+
+def FindH(r,v):
+      return v/(math.pi*pow(r,2))
+
+def FindMinArea(V):
+    minArea = 1000000
+    for i in range(100000):
+        r = (i+1)/10000
+        
+        A = Area(r,V)
+        H = FindH(r,V)
+        if minArea > A:
+            minArea = A
+            minH = H
+            minR = r
+        
+    print("|\t %.2f" % minR, "\t|\t %.2f" %minH ,"\t|\t ",V , "\t|\t %.2f" %(minH/minR)," \t|\t %.2f\t|"  % minArea )
+
+#TODO
+V = 500
+minArea = 100000
+minH = 0
+minR = 0
+print("|\t Rad. \t|\t Hei. \t|\t Vol. \t|\t H/R \t|\t AREA \t| ")
+
+
+FindMinArea(100)
+
+FindMinArea(200)
+
+FindMinArea(300)
+
+FindMinArea(400)
+
+FindMinArea(500)
